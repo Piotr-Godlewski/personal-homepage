efaa8b0ed425c0eaf3785f5e1644d30708f0e301
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-06-01 18:55:00 +0200

    added loremIpsum

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import LoremIpsum from "react-lorem-ipsum";
 import { Container } from "./Container";
 
 function App() {
   return (
     <Container>
-      
+      <LoremIpsum></LoremIpsum>
     </Container>
   );
 }
