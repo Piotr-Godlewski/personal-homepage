191035e7a138b2afbd106b40927c17eb1cb7e45c
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-07-21 19:17:40 +0200

    added styled components

---
+++ src/AboutMe/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledAboutMe = styled.div`
+max-width: 1089px;
+border: 1px solid #D1D5DA4D;
+`;
+
+export const Image = styled.img`
+width: 384px;
+height: 384px;
+border-radius: 50%;
+`;
