36f919b6047864da7c8861619d5228e486103aa0
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-05 20:00:46 +0200

    rebuild component Container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,8 +1,12 @@
-import styled from "styled-components";
+import Tile from "../Tile"
+import { StyledContainer } from "./styled"
 
-export const Container = styled.div`
-max-width: 1216px;
-background-color: ${({ theme }) => theme.color.white};
-box-shadow: 0px 16px 58px 0px #090A3308;
-box-shadow: 0px -2px 50px 0px #090A3305;
-`;
+const Container = () => {
+    return (
+        <StyledContainer>
+            <Tile></Tile>
+        </StyledContainer>
+    )
+}
+
+export default Container
