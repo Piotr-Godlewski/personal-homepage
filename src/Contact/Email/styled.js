6f22f9ad34b5e408cd2d4698b1a284243c3b9233
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-11 17:38:21 +0200

    created component Email

---
+++ src/Contact/Email/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledMail = styled.a`
+text-decoration: none;
+color: ${({theme}) => theme.color.mineShaft};
+font-size: 32px;
+font-style: normal;
+font-weight: 900;
+line-height: normal;
+letter-spacing: 1.6px;
+`
