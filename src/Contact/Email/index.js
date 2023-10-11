2bcb7fdefcab95af8477ee9ead80b01b8b205ab6
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-11 17:37:59 +0200

    created component Email

---
+++ src/Contact/Email/index.js
@@ -0,0 +1,11 @@
+import { StyledMail } from "./styled"
+
+const Email = () => {
+    return (
+        <StyledMail href="mailto:godlewskipiotr.pg@gmail.com">
+            godlewskipiotr.pg@gmail.com
+        </StyledMail>
+    )
+}
+
+export default Email
