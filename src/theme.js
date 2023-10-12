2643cdc5fec4bebca18e054f778b97ea530772e6
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-12 20:03:56 +0200

    added color scienceBlue

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,7 @@ export const theme = {
     color: {
         iron: "#D1D5DA",
         mineShaft: "#252525",
+        scienceBlue: "#0366D6",
         whiteLilac: "#FBFBFE",
         white: "#FFF",
     },
