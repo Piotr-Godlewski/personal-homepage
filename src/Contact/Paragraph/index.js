96eeb6a89d47c3d0030b8743d9bc2ae1139856d5
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-14 17:58:58 +0200

    created paragraph in contact

--- src/Contact/Paragraph/index.js
+++ src/Contact/Paragraph/index.js
@@ -1,6 +1,12 @@
+import { StyledParagraph } from "./styled"
+
 const Paragraph = () => {
     return (
-        <p>Coś o mnie na koniec</p>
+        <StyledParagraph>
+            I am open to new projects.
+            I want to gain experience, so if you have a project for which you need a JS developer: I'm here.🖐
+            Please contact with me, you will be satisfied, I promise.🤞
+        </StyledParagraph>
     )
 }
 
---
+++ src/Contact/Paragraph/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledParagraph = styled.p`
+color: ${({theme}) => theme.color.mineShaft};
+font-family: Inter;
+font-size: 18px;
+font-style: normal;
+font-weight: 400;
+line-height: 140%;
+letter-spacing: 0.9px;
+margin-top: 24px;
+`
