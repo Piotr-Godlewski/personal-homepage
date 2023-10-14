4bb7ac034a2a957b20eaa27a2eca889af1d8b6e3
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-14 17:57:07 +0200

    added new color anakiwa and slateGray

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,10 @@
 export const theme = {
     color: {
+        anakiwa: "#8CC2FF",
         iron: "#D1D5DA",
         mineShaft: "#252525",
         scienceBlue: "#0366D6",
+        slateGray: "#6E7E91",
         whiteLilac: "#FBFBFE",
         white: "#FFF",
     },
