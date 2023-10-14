780e66639579dee0ca770047ef6435ee2e875a83
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-14 17:58:09 +0200

    Added Bio with name, description, button Hire me

--- src/AboutMe/index.js
+++ src/AboutMe/index.js
@@ -1,12 +1,33 @@
-import { StyledAboutMe } from "./styled";
+import { StyledAboutMe, Bio, Header, Name, Description, HireButton } from "./styled";
 import { Image } from "./styled";
 import Photo from "./photo.jpg";
+import { HireIcon } from "./icon/HireIcon";
 
 const AboutMe = () => {
     return (
-        <StyledAboutMe>
-            <Image src={Photo} alt="Piotr Godlewski - photo" />
-        </StyledAboutMe>
+        <>
+            <StyledAboutMe>
+                <Image src={Photo} alt="Piotr Godlewski - photo" />
+                <Bio>
+                    <Header>
+                        This is
+                    </Header>
+                    <Name>
+                        Piotr Godlewski
+                    </Name>
+                    <Description>
+                        I'm a beginner frontend developer 👨🏻‍💻.
+                        I would like to develop my passion ❤ for programming.
+                        See my projects that you will find 🔎 on this website.
+                        I want to become your employee.🙋‍♂️
+                    </Description>
+                    <HireButton href="mailto:godlewskipiotr.pg@gmail.com">
+                        <HireIcon />
+                        Hire Me
+                    </HireButton>
+                </Bio>
+            </StyledAboutMe>
+        </>
     )
 }
 
