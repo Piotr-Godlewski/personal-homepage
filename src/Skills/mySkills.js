9f2bbe786f2503aa16400136073786435cb10e75
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-20 15:02:46 +0200

    updated mySkills

--- src/Skills/mySkills.js
+++ src/Skills/mySkills.js
@@ -1,15 +1,35 @@
 export const mySkills = {
     currently: [
         "HTML",
+        "Responsive Web Design",
+        "JavaScript",
         "React",
-        "React-router",
-        "BEM",
+        "React Router",
+        "React Hooks",
+        "Custom Hooks",
+        "Redux",
+        "Redux-Saga",
+        "Immutability",
+        "Working with API (fetch, axios)",
+        "Error handling",
+        "Promises, Async/Await",
         "CSS",
+        "CSS BEM convention",
+        "CSS Grid",
+        "CSS Flexbox",
         "Trello",
-        "Markdown"
+        "Scrum",
+        "Markdown",
+        "GitHub",
+        "GitHub Pull Requests & Review",
+        "Teamwork"
     ],
     future: [
         "Node.js",
-        "TypeScript"
+        "TypeScript",
+        "React Context",
+        "Unit testing",
+        "JS classes",
+        "Cypress"
     ]
 };
