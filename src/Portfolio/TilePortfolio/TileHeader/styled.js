e355d1fab26064d6dcc6fd15ef04394a365866b0
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-21 19:52:46 +0200

    added styled to TileHeader

---
+++ src/Portfolio/TilePortfolio/TileHeader/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledTileHeader = styled.h3`
+color: var(--Main-blue-LM, #0366D6);
+font-family: Inter;
+font-size: 24px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+letter-spacing: 1.2px;
+`
