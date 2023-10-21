ce028ef8bcede128bd6c4654593c5f21ee840b3a
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-21 19:52:25 +0200

    created TileHeader

---
+++ src/Portfolio/TilePortfolio/TileHeader/index.js
@@ -0,0 +1,11 @@
+import { StyledTileHeader } from "./styled"
+
+const TileHeader = () => {
+    return (
+        <StyledTileHeader>
+            Name that Project
+        </StyledTileHeader>
+    )
+}
+
+export default TileHeader
