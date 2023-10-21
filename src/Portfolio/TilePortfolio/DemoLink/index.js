a18c18f33fae044c47790777144f0d93e0b7cc75
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-21 19:51:14 +0200

    Created DemoLink

---
+++ src/Portfolio/TilePortfolio/DemoLink/index.js
@@ -0,0 +1,12 @@
+import { StyledDemoLink } from "./styled"
+
+const DemoLink = () => {
+    return (
+        <StyledDemoLink>
+            Demo:
+            <a href="https://google.com"> www.linkToDemo.com</a>
+        </StyledDemoLink>
+    )
+}
+
+export default DemoLink
