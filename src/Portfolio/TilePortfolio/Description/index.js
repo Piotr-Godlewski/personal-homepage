87cb2537ec59dbb4c3ad2b08f6924a384b9162bd
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-21 19:51:46 +0200

    created Description

---
+++ src/Portfolio/TilePortfolio/Description/index.js
@@ -0,0 +1,11 @@
+import { StyledDescription } from "./styled"
+
+const Description = () => {
+    return(
+        <StyledDescription>
+            Description about Project
+        </StyledDescription>
+    )
+}
+
+export default Description
