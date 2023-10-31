8d3be2519bd995a67933b803027f4ce2e1de4b2d
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-31 19:34:58 +0100

    added component ToogleMode to Cv

--- src/Cv/index.js
+++ src/Cv/index.js
@@ -3,16 +3,18 @@ import SkillsContainer from "../Skills"
 import Contact from "../Contact"
 import { StyledCv } from "./styled"
 import Portfolio from "../Portfolio"
+import ToogleMode from "../ToogleMode"
 
 const Cv = () => {
     return (
         <>
             <StyledCv>
+                <ToogleMode />
                 <AboutMe>
                 </AboutMe>
                 <SkillsContainer />
                 <Portfolio />
-                <Contact/>
+                <Contact />
             </StyledCv>
         </>
     )
