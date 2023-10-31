cc96e8c8e319ff7433eefeb7a5aad29164e00713
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-10-31 19:37:13 +0100

    added styled-component to ToogleMode

---
+++ src/ToogleMode/styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const StyledToogleMode = styled.div`
+color: var(--Text-secondary, #6E7E91);
+font-family: Inter;
+font-size: 12px;
+font-style: normal;
+font-weight: 700;
+line-height: 130%;
+text-transform: uppercase;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+margin-top: 120px;
+`
+
+export const Button = styled.button`
+background: none;
+border: none;
+cursor: pointer;
+margin-left: 12px;
+display: flex;
+align-items: center;
+`
+
+export const StyledBrightnessBackground = styled.div`
+position: absolute;
+margin: 3px;
+display: flex;
+align-items: center;
+`
+
+export const StyledBrightnessIcon = styled.div`
+position: absolute;
+margin: 6px;
+display: flex;
+align-items: center;
+`
