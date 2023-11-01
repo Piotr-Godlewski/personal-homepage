5ccddd0dfc6d275c575769fd49965c4bccb6814d
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-11-01 12:43:40 +0100

    added media querry

--- src/AboutMe/styled.js
+++ src/AboutMe/styled.js
@@ -5,6 +5,14 @@ max-width: 1089px;
 display: flex;
 gap: 72px;
 align-items: center;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+flex-direction: column;
+align-items: flex-start;
+gap: 16px;
+margin-left: 16px;
+margin-right: 29px;
+};
 `;
 
 export const Image = styled.img`
@@ -12,6 +20,11 @@ width: 398px;
 height: 398px;
 border-radius: 50%;
 margin-left: 10px;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+width: 133px;
+height: 133px;
+}
 `;
 
 export const Bio = styled.div`
@@ -19,7 +32,7 @@ export const Bio = styled.div`
 `;
 
 export const Header = styled.p`
-color: ${({theme}) => theme.color.slateGray};
+color: ${({ theme }) => theme.color.slateGray};
 font-family: Inter;
 font-size: 12px;
 font-style: normal;
@@ -30,7 +43,7 @@ margin: 0;
 `;
 
 export const Name = styled.p`
-color: ${({theme}) => theme.color.mineShaft};
+color: ${({ theme }) => theme.color.mineShaft};
 font-family: Inter;
 font-size: 38px;
 font-style: normal;
@@ -39,10 +52,17 @@ line-height: normal;
 letter-spacing: 1.9px;
 margin-top: 12px;
 margin-bottom: 35px;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+font-size: 22px;
+letter-spacing: 1.1px;
+margin-top: 8px;
+margin-bottom: 16px;
+}
 `;
 
 export const Description = styled.p`
-color: ${({theme}) => theme.color.slateGray};
+color: ${({ theme }) => theme.color.slateGray};
 font-family: Inter;
 font-size: 20px;
 font-style: normal;
@@ -51,6 +71,12 @@ line-height: 140%;
 letter-spacing: 1px;
 margin-bottom: 32px;
 text-align: justify;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+font-size: 17px;
+letter-spacing: 0.85px;
+margin-bottom: 24px;
+}
 `;
 
 export const HireButton = styled.a`
@@ -61,8 +87,8 @@ align-items: center;
 gap: 16px;
 border-radius: 4px;
 border: 1px solid rgba(209, 213, 218, 0.30);
-background: ${({theme}) => theme.color.scienceBlue};
-color: ${({theme}) => theme.color.white};
+background: ${({ theme }) => theme.color.scienceBlue};
+color: ${({ theme }) => theme.color.white};
 text-align: center;
 font-family: Inter;
 font-size: 20.058px;
@@ -74,9 +100,15 @@ text-decoration: none;
 
 &:hover{
     box-shadow: 
-    -2px -2px 0px 0px ${({theme}) => theme.color.anakiwa}, 
-    2px 2px 0px 0px ${({theme}) => theme.color.anakiwa}, 
-    -2px 2px 0px 0px ${({theme}) => theme.color.anakiwa}, 
-    2px -2px 0px 0px ${({theme}) => theme.color.anakiwa};
-}
+    -2px -2px 0px 0px ${({ theme }) => theme.color.anakiwa}, 
+    2px 2px 0px 0px ${({ theme }) => theme.color.anakiwa}, 
+    -2px 2px 0px 0px ${({ theme }) => theme.color.anakiwa}, 
+    2px -2px 0px 0px ${({ theme }) => theme.color.anakiwa};
+};
+
+@media (max-width: ${({theme}) => theme.breakpoint.mobileMax}px){
+font-size: 18px;
+letter-spacing: 0.9px;
+gap: 12px;
+};
 `;
