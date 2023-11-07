d7b9113d51c864b78e0d7c9205da71e8c7f6bd2c
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-11-07 18:45:43 +0100

    added media query

--- src/Portfolio/TilePortfolio/styled.js
+++ src/Portfolio/TilePortfolio/styled.js
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 
 export const StyledTilePortfolio = styled.div`
-width: 592px;
-height: 322px;
 border-radius: 4px;
 border: 6px solid var(--diff-bg-neutral, rgba(209, 213, 218, 0.30));
 background: var(--White, #FFF);
 box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03), 0px -2px 50px 0px rgba(9, 10, 51, 0.02);
-color: ${({theme}) => theme.color.mineShaft};
+color: ${({ theme }) => theme.color.mineShaft};
 padding: 56px;
 
 &:hover {
@@ -16,4 +14,10 @@ border: 6px solid rgba(3, 102, 214, 0.20);
 background: var(--White, #FFF);
 box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03), 0px -2px 50px 0px rgba(9, 10, 51, 0.02);
 };
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+padding: 24px;
+width: 100%;
+height: auto;
+}
 `
