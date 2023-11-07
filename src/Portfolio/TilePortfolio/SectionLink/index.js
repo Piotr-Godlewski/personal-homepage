deb471ba7ad3d4856c306d683cf975b3d8adbb1f
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-11-07 18:46:20 +0100

    added new section - SectionLink

---
+++ src/Portfolio/TilePortfolio/SectionLink/index.js
@@ -0,0 +1,13 @@
+import CodeLink from "./CodeLink"
+import DemoLink from "./DemoLink"
+
+const SectionLink = () => {
+    return (
+        <>
+            <DemoLink />
+            <CodeLink />
+        </>
+    )
+}
+
+export default SectionLink
